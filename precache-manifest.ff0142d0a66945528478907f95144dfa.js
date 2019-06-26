d16387f75d42669d56bafc0e8e391ef54bd4c089
kenmaready <ken@maready.com>
2019-06-26 17:17:15 -0400

    Updates

---
+++ precache-manifest.ff0142d0a66945528478907f95144dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b397d093a7edcd7505c88abed16ab80",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "9ccc93937171dc69009e",
+    "url": "/quote-machine/static/css/main.e8a44ea3.chunk.css"
+  },
+  {
+    "revision": "d4f41127f0901439aedd",
+    "url": "/quote-machine/static/js/2.ad91a147.chunk.js"
+  },
+  {
+    "revision": "9ccc93937171dc69009e",
+    "url": "/quote-machine/static/js/main.8e17e73c.chunk.js"
+  },
+  {
+    "revision": "bc2b7c61d93870784a32",
+    "url": "/quote-machine/static/js/runtime~main.85834c65.js"
+  }
+]);
